41f9cfbb93431679fe6b702209646120723395c1
6704062616231 <s6704062616231@email.kmutnb.ac.th>
2025-12-15 16:17:21 +0700

    case 6B

---
+++ lab3.6B.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	int score;
+	scanf("%d",&score);
+	if (score < 68){
+		if(score >= 55 ){
+			printf("D");
+		}else {
+			printf("F");
+		}
+	}else{
+		if(score>=85){
+			printf("A");
+		}else if(score >= 75){
+			printf("B");
+		}else if(score >= 68){
+			printf("C");
+		}
+	}
+	
+
+return 0;
+}
